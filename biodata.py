e04f81f33d6a18b30daeca173056be66c862baaa
dimas007-nub <82008072+dimas007-nub@users.noreply.github.com>
2021-04-06 07:32:57 +0530

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,6 @@
+print("=============================")
+print("     Teknik Informatika      ")
+print("=============================")
+print("Nama  : Dimas Bayu Cahya Putra")
+print("Nim   : B34180015")
+print("Prodi : Teknik Informatika 2018 A")
